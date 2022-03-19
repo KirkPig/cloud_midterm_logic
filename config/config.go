5e95f467c1350675f2c27d71958aa51e800e8f79
Nut Pinyo <bomb.np@gmail.com>
2022-03-19 23:35:43 +0700

    feat: add model, db connection, app configuration

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"log"
+	"strings"
+	"sync"
+
+	"github.com/KirkPig/cloud_midterm_logic/postgres"
+	"github.com/spf13/viper"
+)
+
+var configOnce sync.Once
+var config *Config
+
+type Config struct {
+	Postgres postgres.Config `mapstructure:"postgres"`
+}
+
+func InitConfig() *Config {
+	configOnce.Do(func() {
+
+		viper.SetConfigName("config") // name of config file without extension
+		viper.AddConfigPath("./config") // path to look for config file
+
+
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+		if err := viper.ReadInConfig(); err != nil {
+			log.Fatal("Config file not found", err.Error())
+		}
+		viper.AutomaticEnv()
+
+		viper.WatchConfig() // Watch for changes to the configuration file and recompile
+		if err := viper.Unmarshal(&config); err != nil {
+			panic(err)
+		}
+		log.Println("Config initialized!")
+	})
+	return config
+}
--- repository/model.go
+++ repository/model.go
@@ -1,1 +1,14 @@
 package repository
+
+import "time"
+
+type Message struct {
+	uuid              string     `gorm:"column:uuid;primary_key"`
+	author            string     `gorm:"column:author"`
+	message           string     `gorm:"column:message"`
+	likes             int        `gorm:"column:likes"`
+	lastUpdateAuthor  *time.Time `gorm:"column:last_update_author"`
+	lastUpdateMessage *time.Time `gorm:"column:last_update_message"`
+	lastUpdateLikes   *time.Time `gorm:"column:last_update_likes"`
+	isDeleted         bool       `gorm:"column:is_deleted"`
+}
